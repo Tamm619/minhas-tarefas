a11cd95ea6d3bee6a5d7ae14f822c3872ccfc360
Mateus Gonçalves <mateusenrique123@gmail.com>
2026-01-03 10:32:06 -0300

    Projeto 5 parte 1

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,20 @@
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard legenda="Pendentes" contador={1} />
+        <FiltroCard legenda="Concluídas" contador={2} />
+        <FiltroCard legenda="Urgentes" contador={3} />
+        <FiltroCard legenda="Importantes" contador={4} />
+        <FiltroCard legenda="Normal" contador={5} />
+        <FiltroCard legenda="Buscar" contador={6} />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
+
+export default BarraLateral
